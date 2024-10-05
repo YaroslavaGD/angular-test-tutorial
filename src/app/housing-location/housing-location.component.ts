8512148d3d2945575b21c8f0429d26fc9a86586b
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2024-10-05 14:49:07 +0200

    feat: add data in home-component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
