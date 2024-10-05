31baf5bdb2e961e4f706907b8a4d067e21feff7b
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2024-10-05 16:07:33 +0200

    feat: add routes

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
