df421fc8a0783c7cd41701d695a5917c2260e310
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2024-10-06 19:04:11 +0200

    feat: add promise-functions in housing-service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,8 +16,10 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocation();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocation().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = this.housingLocationList;
+    });
   }
 
   filterResults(text: string) {
